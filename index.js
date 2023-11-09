99b23f4868614de3dce6d80acd2a300f38cf7ea3
Sheetal Pujari <pujari.sh@northeastern.edu>
2023-11-08 20:24:20 -0500

    added index.js

--- index.js
+++ index.js
@@ -159,73 +159,40 @@ aws.getAvailabilityZones({ state: state }).then((data) => {
         cidrBlocks: ["0.0.0.0/0"],
       },
     ],
-  });
-
-    const rdsParameterGroup = new aws.rds.ParameterGroup("customrdsparamgroup", {
-      family: "mysql8.0", // Replace with the appropriate RDS engine and version
-      parameters: [
-          {
-              name: "max_connections",
-              value: "100",
-          },
-          // Add more parameters as needed
-      ],
-  });
-
-  
-
-    const dbsubnetgroup = new aws.rds.SubnetGroup("rdssubnetgroup", {
-        subnetIds: privateSubnets.map(subnet => subnet.id),
-        description:"My rds subnet group for private subnets",
-    });
+    egress: [
+      {
+        fromPort: 3306,      // Allow outbound traffic on port 3306
+        toPort: 3306,        // Allow outbound traffic on port 3306
+        protocol: "tcp",     // TCP protocol
+        cidrBlocks: ["0.0.0.0/0"],  // Allow all destinations
+      },
+    ],
     
-    const webAppSecurityGroup = new aws.ec2.SecurityGroup("webapp-sg", {
-      vpcId: vpc.id,
-      ingress: [
+  })
+  const rdsParameterGroup = new aws.rds.ParameterGroup("customrdsparamgroup", {
+    family: "mysql8.0", // Replace with the appropriate RDS engine and version
+    parameters: [
         {
-          fromPort: 22,
-          toPort: 22,
-          protocol: "tcp",
-          cidrBlocks: ["0.0.0.0/0"], // Allow SSH from anywhere
+            name: "max_connections",
+            value: "100",
         },
-        {
-          fromPort: 80,
-          toPort: 80,
-          protocol: "tcp",
-          cidrBlocks: ["0.0.0.0/0"], // Allow HTTP from anywhere
-        },
-        {
-          fromPort: 443,
-          toPort: 443,
-          protocol: "tcp",
-          cidrBlocks: ["0.0.0.0/0"], // Allow HTTPS from anywhere
-        },
-        {
-          fromPort: 8080,
-          toPort: 8080,
-          protocol: "tcp",
-          cidrBlocks: ["0.0.0.0/0"], // Allow your application traffic from anywhere
-        },
-      ],
-      egress: [
-        {
-          fromPort: 3306,      // Allow outbound traffic on port 3306
-          toPort: 3306,        // Allow outbound traffic on port 3306
-          protocol: "tcp",     // TCP protocol
-          cidrBlocks: ["0.0.0.0/0"],  // Allow all destinations
-        },
-     
-      ],
+        // Add more parameters as needed
+    ],
+    
+    
     });
-   
-    const dbSecurityGroup = new aws.ec2.SecurityGroup("db-sg", {
+   const dbsubnetgroup = new aws.rds.SubnetGroup("rdssubnetgroup", {
+      subnetIds: privateSubnets.map(subnet => subnet.id),
+      description:"My rds subnet group for private subnets",
+  })
+  const dbSecurityGroup = new aws.ec2.SecurityGroup("db-sg", {
       vpcId: vpc.id,
       ingress: [
           {
               fromPort: 3306, // For MariaDB
               toPort: 3306, // For MariaDB
               protocol: "tcp",
-              securityGroups: [webAppSecurityGroup.id], // Referencing the application security group as source
+              securityGroups: [securityGroup.id], // Referencing the application security group as source
           },
       ],
       egress: [
@@ -240,7 +207,7 @@ aws.getAvailabilityZones({ state: state }).then((data) => {
     const rdsInstance = new aws.rds.Instance('my-rds-instance', {
       allocatedStorage: 20,
       storageType: 'gp2',
-      engine: 'mysql', // Replace with 'postgres' or 'mariadb' as needed
+      engine: "mysql", // Replace with 'postgres' or 'mariadb' as needed
       instanceClass: 'db.t2.micro', // Use the cheapest available class
       name: 'csye6225',
       username:'csye6225',
@@ -251,9 +218,32 @@ aws.getAvailabilityZones({ state: state }).then((data) => {
       publiclyAccessible: false, // No public accessibility
       vpcSecurityGroupIds: [dbSecurityGroup.id], // Replace with the ID of your Database security group
       parameterGroupName:rdsParameterGroup,
+  });
   
 
-  });
+  const cloudWatchAgentRole = new aws.iam.Role("AgentRole", {
+    assumeRolePolicy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [{
+            Action: "sts:AssumeRole",
+            Effect: "Allow",
+            Principal: {
+                Service: "ec2.amazonaws.com"
+            }
+        }]
+    })
+});
+const rolePolicyAttachment = new aws.iam.RolePolicyAttachment("cloudwatchAgentPolicyAttachment", {
+  policyArn: "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
+  role: cloudWatchAgentRole.name,
+
+});
+const instanceProfile = new aws.iam.InstanceProfile(
+  "instanceProfileName", {
+  role: cloudWatchAgentRole.name,
+  dependsOn: [rolePolicyAttachment] 
+ 
+});
   const ec2Inst = new aws.ec2.Instance(instanceName, {
     ami:aws.ec2.getAmi({
       owners: [owner],
@@ -261,29 +251,51 @@ aws.getAvailabilityZones({ state: state }).then((data) => {
       filters: [{ name: "state", values: ["available"] }],
     }).then((ami) => ami.id),
     dependsOn:rdsInstance,
+    iamInstanceProfile: instanceProfile.name,
     userData:pulumi.interpolate `
     #!/bin/bash
-    cd /home/admin
+    cd /home/admin/WebApp
     chmod +w .env
-    host="${rdsInstance.address}"
-    user="${rdsInstance.username}",
-    dbname="${rdsInstance.dbName}"
-    password="${rdsInstance.password}"
-    port="${rdsInstance.port}"
-    db_dialect="${rdsInstance.engine}"
-    
-    # Edit the key-value pairs
-    echo "HOST=$host" > .env
-    echo "DATABASE_USER=$user" >> .env
-    echo "DATABASE_NAME=$dbname" >> .env
-    echo "DATABASE_PASSWORD=$password" >> .env
-    echo "DATABASE_PORT=$port" >> .env
-    `,
+    editable_file=".env"  
+    mysql_database=${rdsInstance.dbName}
+    mysql_user=${rdsInstance.username}
+    mysql_password=${rdsInstance.password}
+    mysql_port=${rdsInstance.port}
+    mysql_host=${rdsInstance.address}
+    db_dialect=${rdsInstance.engine}
+       
+    if [ -f "$editable_file" ]; then
+           
+    > "$editable_file"
+       
+        # Add new key-value pairs
+        echo "MYSQL_DATABASE=$mysql_database" >> "$editable_file"
+        echo "MYSQL_USER=$mysql_user" >> "$editable_file"
+        echo "MYSQL_PASSWORD=$mysql_password" >> "$editable_file"
+        echo "MYSQL_PORT=$mysql_port" >> "$editable_file"
+        echo "MYSQL_HOST=$mysql_host" >> "$editable_file"
+        echo "DB_DIALECT=$db_dialect" >> "$editable_file"
+       
+        echo "Cleared old data in $editable_file and added new key-value pairs."
+    else
+        echo "File $editable_file does not exist."
+    fi
+    sudo chown csye6225:csye6225 /home/admin/WebApp
+ 
+    sudo chmod 750 /home/admin/WebApp
+
+    sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
+
+    `.apply((s)=>s.trim()),
     instanceType: "t2.micro",
     vpcSecurityGroupIds: [securityGroup.id],
     associatePublicIpAddress: true,
     subnetId: publicSubnets[0].id,
-    keyName: "ec2-key",
+    keyName: "ec2dev",
     tags: { Name: instanceName },
     rootBlockDevice: {
       volumeSize: 25,
@@ -291,4 +303,19 @@ aws.getAvailabilityZones({ state: state }).then((data) => {
       deleteOnTermination: true,
     },
   });
+  //for A record creation
+    //const baseDomainName = config.require("basedomain"); 
+    const baseDomainName = "dev.sheetalpujari.me";
+    const zonePromise = aws.route53.getZone({ name: baseDomainName }, { async: true });
+
+    zonePromise.then(zone => {
+
+    const record = new aws.route53.Record("myRecord", {
+    zoneId: zone.zoneId, 
+    name: "",
+    type: "A",
+    ttl: 60,
+    records: [ec2Inst.publicIp],
+}, { dependsOn: [ec2Inst]});
+});
 });
